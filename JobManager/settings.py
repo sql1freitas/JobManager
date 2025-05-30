3d0b7a940cd0472483920153394ba57055338f25
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2025-05-30 11:11:49 -0300

    Secret Key modificada para publicação do repositório

--- JobManager/settings.py
+++ JobManager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&=96-_*hfgag=v4d0us6%%wna#wjdll^icl8sf9@3g!+6j_rpo'
+SECRET_KEY = 'insert here comes your secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
